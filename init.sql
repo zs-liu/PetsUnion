871426fa7efd2596b399b14a1027c3249357d426
zs-liu <zsliu98@gmail.com>
2018-11-04 22:29:49 +0800

    Correct init.sql

--- init.sql
+++ init.sql
@@ -1,35 +1,52 @@
-CREATE DATABASE `petsunion` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `petsunion` CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE petsunion;
+
+DROP TABLE IF EXISTS shops;
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS services;
+DROP TABLE IF EXISTS orders;
+
 CREATE TABLE IF NOT EXISTS shops (
-    id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id int(11) UNSIGNED AUTO_INCREMENT,
     name varchar(40) NOT NULL,
     password char(32) NOT NULL,
     address varchar(40),
-    picture varchar(40)
+    picture varchar(40),
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
 )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS pets (
-    id int(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name varchar(20) NOT NULL
+    id int(5) UNSIGNED AUTO_INCREMENT,
+    name varchar(20) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
 )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS owners (
-    id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id int(11) UNSIGNED AUTO_INCREMENT,
     name varchar(40) NOT NULL,
     password char(32) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
 )
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS services (
-    id bigint(21) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    shop int(11) NOT NULL,
-    pet int(5) NOT NULL,
+    id bigint(21) UNSIGNED AUTO_INCREMENT,
+    shop int(11) UNSIGNED,
+    pet int(5) UNSIGNED,
     name varchar(20) NOT NULL,
     start_time TIME NOT NULL,
-    end_time TIME NOT NULL
+    end_time TIME NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id),
     CONSTRAINT belong_shop
     FOREIGN KEY (shop)
     REFERENCES shops (id),
@@ -41,16 +58,18 @@ CREATE TABLE IF NOT EXISTS services (
     DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS orders (
-    id bigint(21) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id bigint(21) UNSIGNED AUTO_INCREMENT,
     service bigint(21) UNSIGNED NOT NULL,
     owner int(11) UNSIGNED NOT NULL,
     order_time DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id),
     CONSTRAINT service_id
     FOREIGN KEY (service)
     REFERENCES services (id),
     CONSTRAINT owner_id
     FOREIGN KEY (owner)
-    REFERENCES owners (id),
+    REFERENCES owners (id)
 )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8;
