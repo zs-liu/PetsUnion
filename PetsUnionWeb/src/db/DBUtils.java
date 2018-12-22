d0580b2e204d0e36f05e01606b181c5b6fa829bd
zs-liu <zsliu98@gmail.com>
2018-12-22 14:32:15 +0800

    Set default character

--- PetsUnionWeb/src/db/DBUtils.java
+++ PetsUnionWeb/src/db/DBUtils.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class DBUtils {
 
     public static Connection getConn() {
-        String url = "jdbc:mysql://localhost:3306/demo";
+        String url = "jdbc:mysql://localhost:3306/demo?characterEncoding=utf8";
         String username = "root";
         String password = "LzscsL"; // use your configuration
 
